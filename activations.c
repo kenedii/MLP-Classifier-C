0716ae3d720b4790b157d3816a2626d605014245
kenedii <55466055+kenedii@users.noreply.github.com>
2025-02-02 16:34:28 -0500

    derivatives of activation functions removed
    they were already being computed in the backprop.c file, so we don't need them twice

--- activations.c
+++ activations.c
@@ -1,21 +1,11 @@
-// Contains activation functions and their derivatives
+// Contains activation functions
 
 float sigmoid(float x)
 {
     return 1 / (1 + exp(-x));
 }
 
-float dsigmoid(float x)
-{
-    return sigmoid(x) * (1 - sigmoid(x));
-}
-
 float relu(float x)
 {
     return x > 0 ? x : 0;
-}
-
-float drelu(float x)
-{
-    return x > 0 ? 1 : 0;
 }
